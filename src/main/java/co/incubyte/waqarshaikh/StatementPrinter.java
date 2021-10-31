123134ec95d24cd87bc960627a1bf981474716ae
waqar <waqar@incubyte.co>
2021-10-31 17:44:01 +0530

    Pass the failing StatementPrinter Unit Test

--- src/main/java/co/incubyte/waqarshaikh/StatementPrinter.java
+++ src/main/java/co/incubyte/waqarshaikh/StatementPrinter.java
@@ -1,16 +1,41 @@
 package co.incubyte.waqarshaikh;
 
+import static java.util.stream.Collectors.toCollection;
+
+import java.text.DecimalFormat;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class StatementPrinter {
 
-  private final Console console;
+  private DecimalFormat decimalFormater = new DecimalFormat("#.00");
+  private Console console;
 
   public StatementPrinter(Console console) {
     this.console = console;
   }
 
   public void print(List<Transaction> transactions) {
-    throw new UnsupportedOperationException();
+    console.printLine("DATE | AMOUNT | BALANCE");
+
+    printStatementLines(transactions);
+  }
+
+  private void printStatementLines(List<Transaction> transactions) {
+    AtomicInteger runningBalance = new AtomicInteger(0);
+    transactions.stream()
+        .map(transaction -> statementLine(transaction, runningBalance))
+        .collect(toCollection(LinkedList::new))
+        .descendingIterator()
+        .forEachRemaining(console::printLine);
+  }
+
+  private String statementLine(Transaction transaction, AtomicInteger runningBalance) {
+    return transaction.date() +
+        " | " +
+        decimalFormater.format(transaction.amount()) +
+        " | " +
+        decimalFormater.format(runningBalance.addAndGet(transaction.amount()));
   }
 }
